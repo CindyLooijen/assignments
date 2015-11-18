e77729467dae781a1e209aab8db4dbdbefc98b30
Cindy <cindyrolina@hotmail.com>
2015-11-18 19:00:41 +0000

    assignment 3

--- functions.js
+++ functions.js
@@ -10,6 +10,7 @@ alert("I would suggest you leave this page...");
 
 var image = document.getElementById('image'); 
 
+//function to turn the image. When the angle reaches 360 degrees it goes back to 0
 var angle = 0;
 var turnImage = function() {
     angle+=90;
@@ -20,6 +21,7 @@ var turnImage = function() {
     }
 };
 
+// button to check whether you have the right side up
 var Guess = function () {
     if(angle === 0) {
         alert("Correct!!");
@@ -29,40 +31,35 @@ var Guess = function () {
     }
 };
 
-var images = [
-src="http://corioblog.com/Mondrian.jpg", 
-src="https://classconnection.s3.amazonaws.com/788/flashcards/412788/png/square_comp_with_horse1315435198157.png", 
-src="http://generousmagpie.com/assets/beeldende-kunst-jackson-pollock-erik-schilp-315x315.jpg",
-src="http://lh6.ggpht.com/B5fwkTesTCio6C9Zc9tOB6-eVRYhTyBBMsZWr2j44BTfTIPBOB4mxh-xIB83Ce3yOUEfzvem2_xWCsOsI5g8AkwpPw=s500-c",
-src="http://images.fineartamerica.com/images-medium-large-5/abstract-rainbow-bliss-fractal-square-andee-photography.jpg",
-src="https://upload.wikimedia.org/wikipedia/commons/6/63/Robert_Delaunay,_1913,_Premier_Disque,_134_cm,_52.7_inches,_Private_collection.jpg",
-src="http://blog.ssis.edu.vn/105976/wp-content/uploads/sites/449/2015/02/48x48-hitchcock-vertigo-huge-signed-art-abstract-paintings-modern-wwwsplashyartistcom-robert-r-abstract-art.jpg",
-src="http://static1.squarespace.com/static/5511fc7ce4b0a3782aa9418b/t/553695e6e4b05a14c74e79eb/1429640680022/abstract-modern-art.jpg",
-src="http://images.monzuki.com/blog/images/images_October2009/PatrickGunderson/PatrickGunderson_03.jpg",
-src="http://www.art-spire.com/wp-content/gallery/05_09_09-Patrick%20Gunderson/896911234161395.jpg"
-];
+//creating an empty array
+var images = new Array (24);
+
 var angle = 0;
 var imageNumber = 0;
 
+// previous button to go back an image
+var prev = function(){
+    imageNumber-=1;
+    if (imageNumber === -1){
+        imageNumber = images.length-1;
+    } //function to randomly turn the image to make the game more challanging
+    turnImage();  
+    image.src = images[imageNumber];
+    console.log("prev received");
+};
+
+// next button to go to the next image
 var next = function(){
     imageNumber+=1;
     if (imageNumber === images.length){
         imageNumber=0;
-    }
+    } //function to randomly turn the image to make the game more challanging
     turnImage();  
     image.src = images[imageNumber];
     console.log("next received");
 };
 
-var prev = function(){
-    imageNumber-=1;
-    if (imageNumber === -1){
-        imageNumber = images.length-1;
-    }
-    turnImage();  
-    image.src = images[imageNumber];
-    console.log("prev received");
-};
+
 
 
     
